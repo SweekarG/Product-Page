2b73166d8bc67eddc3f83dfeeea969a5a36f2703
SweekarG <np05cp4s240071@iic.edu.np>
2024-05-13 13:35:49 +0545

    add to cart done

--- js/script.js
+++ js/script.js
@@ -62,117 +62,135 @@ snowflakesTab.addEventListener("click", function (e) {
 const products1 = [
   {
     id: 1,
-    name: "John Doe",
+    name: "Milk",
     beforePrice: 500,
     price: 200,
-    image: "images/buff.jpg",
+    image: "images/milk.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
   {
     id: 2,
-    name: "Jane Doe",
+    name: "Ghee",
     beforePrice: 500,
     price: 300,
-    image: "images/chicken.jpg",
+    image: "images/ghee.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
   {
     id: 3,
-    name: "John Smith",
+    name: "butter",
     beforePrice: 500,
     price: 400,
-    image: "images/pork.jpg",
+    image: "images/butter.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
   {
     id: 4,
-    name: "Jane Smith",
+    name: "cheese",
     beforePrice: 500,
     price: 500,
-    image: "images/mutton.jpg",
+    image: "images/cheese.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 5,
+    name: "Curd",
+    beforePrice: 500,
+    price: 500,
+    image: "images/curd.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 6,
+    name: "Milk shake",
+    beforePrice: 500,
+    price: 500,
+    image: "images/milkshake.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 7,
+    name: "Ice Cream",
+    beforePrice: 500,
+    price: 500,
+    image: "images/icecream.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 8,
+    name: "Lassi",
+    beforePrice: 500,
+    price: 500,
+    image: "images/lassi.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
 ];
 const products2 = [
   {
-    id: 5,
-    name: "John Doe",
-    beforePrice: 500,
-    price: 200,
-    image: "images/buff.jpg",
-    quantity: "2kg",
-    reviews: "4.9 (5 reviews)",
-  },
-  {
-    id: 6,
-    name: "Jane Doe",
-    beforePrice: 500,
-    price: 300,
-    image: "images/chicken.jpg",
-    quantity: "2kg",
-    reviews: "4.9 (5 reviews)",
-  },
-  {
-    id: 7,
-    name: "John Smith",
-    beforePrice: 500,
-    price: 400,
-    image: "images/pork.jpg",
-    quantity: "2kg",
-    reviews: "4.9 (5 reviews)",
-  },
-  {
-    id: 8,
-    name: "Jane Smith",
-    beforePrice: 500,
-    price: 500,
-    image: "images/mutton.jpg",
-    quantity: "2kg",
-    reviews: "4.9 (5 reviews)",
-  },
-  {
     id: 9,
-    name: "Jane Smith",
-    beforePrice: 500,
-    price: 500,
-    image: "images/mutton.jpg",
-    quantity: "2kg",
+    name: "Coconut Oil",
+    beforePrice: 250,
+    price: 270,
+    image: "images/coconutoil.jpg",
+    quantity: "1ltr",
     reviews: "4.9 (5 reviews)",
   },
   {
     id: 10,
-    name: "Jane Smith",
-    beforePrice: 500,
-    price: 500,
-    image: "images/mutton.jpg",
-    quantity: "2kg",
+    name: "mustard Oil",
+    beforePrice: 315,
+    price: 300,
+    image: "images/mustard.jpg",
+    quantity: "1ltr",
     reviews: "4.9 (5 reviews)",
   },
   {
     id: 11,
-    name: "Jane Smith",
+    name: "Milk",
     beforePrice: 500,
-    price: 500,
-    image: "images/mutton.jpg",
-    quantity: "2kg",
+    price: 400,
+    image: "images/milk.jpg",
+    quantity: "5ltr",
     reviews: "4.9 (5 reviews)",
   },
   {
     id: 12,
-    name: "Jane Smith",
+    name: "Butter",
     beforePrice: 500,
     price: 500,
-    image: "images/mutton.jpg",
+    image: "images/butter.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
   {
     id: 13,
-    name: "Jane Smith",
+    name: "Milk shake",
+    beforePrice: 500,
+    price: 500,
+    image: "images/milkshake.jpg",
+    quantity: "350 ml",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 14,
+    name: "fish",
+    beforePrice: 500,
+    price: 500,
+    image: "images/fish.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 15,
+    name: "Mutton",
     beforePrice: 500,
     price: 500,
     image: "images/mutton.jpg",
@@ -180,95 +198,185 @@ const products2 = [
     reviews: "4.9 (5 reviews)",
   },
   {
-    id: 14,
-    name: "Jane Smith",
+    id: 16,
+    name: "Sausages",
     beforePrice: 500,
     price: 500,
-    image: "images/mutton.jpg",
+    image: "images/sausage.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 17,
+    name: "corn",
+    beforePrice: 500,
+    price: 500,
+    image: "images/corn.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 18,
+    name: "Lassi",
+    beforePrice: 500,
+    price: 500,
+    image: "images/lassi.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
 ];
 const products3 = [
   {
-    id: 15,
-    name: "John Doe",
+    id: 19,
+    name: "apples",
     beforePrice: 500,
     price: 200,
-    image: "images/buff.jpg",
+    image: "images/apples.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
   {
-    id: 16,
-    name: "Jane Doe",
+    id: 20,
+    name: "potatoes",
     beforePrice: 500,
     price: 300,
-    image: "images/chicken.jpg",
+    image: "images/potato.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
   {
-    id: 17,
-    name: "John Smith",
+    id: 21,
+    name: "Tomato",
     beforePrice: 500,
     price: 400,
-    image: "images/buff.jpg",
+    image: "images/tomato.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
   {
-    id: 18,
-    name: "Jane Smith",
+    id: 22,
+    name: "Pumpkin",
     beforePrice: 500,
     price: 500,
-    image: "images/mutton.jpg",
+    image: "images/pumpkin.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 23,
+    name: "capsicum",
+    beforePrice: 500,
+    price: 500,
+    image: "images/capsicum.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 24,
+    name: "grapes",
+    beforePrice: 500,
+    price: 500,
+    image: "images/grapes.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 25,
+    name: "Cauliflower",
+    beforePrice: 500,
+    price: 500,
+    image: "images/cauli.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 26,
+    name: "Guava",
+    beforePrice: 500,
+    price: 500,
+    image: "images/guava.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
 ];
 const products4 = [
   {
-    id: 19,
-    name: "John Doe",
+    id: 27,
+    name: "Rice",
     beforePrice: 500,
     price: 200,
-    image: "images/buff.jpg",
+    image: "images/rice.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
   {
-    id: 20,
-    name: "Jane Doe",
+    id: 28,
+    name: "Wheat",
     beforePrice: 500,
     price: 300,
-    image: "images/chicken.jpg",
+    image: "images/wheat.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
   {
-    id: 21,
-    name: "John Smith",
+    id: 29,
+    name: "lentitls",
     beforePrice: 500,
     price: 400,
-    image: "images/fish.jpg",
+    image: "images/lentils.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
   {
-    id: 22,
-    name: "Jane Smith",
+    id: 30,
+    name: "chick Peas",
     beforePrice: 500,
     price: 500,
-    image: "images/mutton.jpg",
+    image: "images/chickpeas.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 31,
+    name: "Buck Wheat",
+    beforePrice: 500,
+    price: 500,
+    image: "images/buckwheat.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 32,
+    name: "Corn",
+    beforePrice: 500,
+    price: 500,
+    image: "images/corn.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 33,
+    name: "Oats",
+    beforePrice: 500,
+    price: 500,
+    image: "images/oats.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 34,
+    name: "Kidney beans",
+    beforePrice: 500,
+    price: 500,
+    image: "images/kidneybeans.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
 ];
 const products5 = [
   {
-    id: 19,
-    name: "John Doe",
+    id: 35,
+    name: "Buff",
     beforePrice: 500,
     price: 200,
     image: "images/buff.jpg",
@@ -276,8 +384,8 @@ const products5 = [
     reviews: "4.9 (5 reviews)",
   },
   {
-    id: 20,
-    name: "Jane Doe",
+    id: 36,
+    name: "Chicken",
     beforePrice: 500,
     price: 300,
     image: "images/chicken.jpg",
@@ -285,8 +393,8 @@ const products5 = [
     reviews: "4.9 (5 reviews)",
   },
   {
-    id: 21,
-    name: "John Smith",
+    id: 37,
+    name: "Fish",
     beforePrice: 500,
     price: 400,
     image: "images/fish.jpg",
@@ -294,49 +402,85 @@ const products5 = [
     reviews: "4.9 (5 reviews)",
   },
   {
-    id: 22,
-    name: "Jane Smith",
+    id: 38,
+    name: "Mutton",
     beforePrice: 500,
     price: 500,
     image: "images/mutton.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 39,
+    name: "Sausages",
+    beforePrice: 500,
+    price: 500,
+    image: "images/sausage.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 40,
+    name: "Pork",
+    beforePrice: 500,
+    price: 500,
+    image: "images/pork.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 41,
+    name: "Bacon",
+    beforePrice: 500,
+    price: 500,
+    image: "images/bacon.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
 ];
 const products6 = [
   {
-    id: 19,
-    name: "John Doe",
+    id: 42,
+    name: "Coconut Oil",
     beforePrice: 500,
     price: 200,
-    image: "images/buff.jpg",
+    image: "images/coconutoil.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
   {
-    id: 20,
-    name: "Jane Doe",
+    id: 43,
+    name: "Apple Cider Vinegar",
     beforePrice: 500,
     price: 300,
-    image: "images/chicken.jpg",
+    image: "images/applevinegar.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
   {
-    id: 21,
-    name: "John Smith",
+    id: 44,
+    name: "Sunflower Oil",
     beforePrice: 500,
     price: 400,
-    image: "images/fish.jpg",
+    image: "images/sunflower.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
   {
-    id: 22,
-    name: "Jane Smith",
+    id: 45,
+    name: "Olive Oil",
     beforePrice: 500,
     price: 500,
-    image: "images/mutton.jpg",
+    image: "images/olive.jpg",
+    quantity: "2kg",
+    reviews: "4.9 (5 reviews)",
+  },
+  {
+    id: 46,
+    name: "Corn Oil",
+    beforePrice: 500,
+    price: 500,
+    image: "images/cornoil.jpg",
     quantity: "2kg",
     reviews: "4.9 (5 reviews)",
   },
@@ -370,7 +514,7 @@ const addDataToHTML = (products, container) => {
     newProduct.classList.add("products__desc");
     newProduct.innerHTML = `<img src="${product.image}" alt="${product.name}" class="products__img">
       <h4 class="products__title">${product.name}<p class= "quantity">${product.quantity}</p></h4>
-      <div class = "star"><img src="img/star.png" alt="star" class="star__img">${product.reviews}</div>
+      <div class = "star"><img src="images/star.png" alt="star" class="star__img">${product.reviews}</div>
       <h4 class= "price"><p><del>${product.beforePrice}</del></p>${product.price}</h4>
       <button class="btn btn--cart">+ Add to Cart</button>`;
     container.appendChild(newProduct);
@@ -413,7 +557,7 @@ const productsAll = [
   products6,
 ];
 let listCart = document.querySelector(".list-cart");
-let cartSpan = document.querySelector(".list-cart span");
+let cartSpan = document.querySelector(".icon-cart span");
 
 let carts = [];
 
@@ -426,6 +570,7 @@ productDesc.forEach((product) => {
       let products_id = positionClick.parentElement.dataset.id;
 
       addToCartFunction(products_id);
+      addCartToHTML();
     }
   });
 });
@@ -450,37 +595,48 @@ const addToCartFunction = (products_id) => {
     carts[positionInCart].quantity += 1;
   }
   console.log(carts);
+  addCartToHTML();
 };
 
 const addCartToHTML = () => {
-  modal.innerHTML = "";
-  if (carts.length > 0) {
-    carts.forEach((cart) => {
-      let product = document.createElement("div");
-      product.classList.add("cart-item");
-      productsAll.forEach((products) => {
-        let productData = productsAll.find(
-          (product) => product.id == cart.products_id
-        );
-        if (productData) {
-          let info = productsAll[productData];
-          product.innerHTML = ` <div class = "cart-item">
+  listCart.innerHTML = "";
+  let totalQuantity = 0;
+  carts.forEach((cart, index) => {
+    totalQuantity += cart.quantity;
+    let productData;
+    for (let i = 0; i < productsAll.length; i++) {
+      productData = productsAll[i].find(
+        (product) => product.id == cart.products_id
+      );
+      if (productData) break;
+    }
+    if (productData) {
+      let cartItem = document.createElement("div");
+      cartItem.classList.add("cart-item");
+      cartItem.innerHTML = ` <div class = "cart-item">
       <div class="cart-item__image">
-        <img src="${info.image}" class = "products__img--cart"alt="Vegetable" />
+        <img src="${
+          productData.image
+        }" class = "products__img--cart"alt="Vegetable" />
         </div>
-        <div class="cart-item__name">${info.name}</div>
-        <div class="cart-item__price">Rs${info.price * cart.quantity}</div>
+        <div class="cart-item__name">${productData.name}</div>
+        <div class="cart-item__price">Rs${
+          productData.price * cart.quantity
+        }</div>
          <div class="cart-item__quantity">
            <span class="minus"><</span>
            <span>${cart.quantity}</span>
            <span class="add">></span>
          </div>
        </div>`;
-          listCart.appendChild(product);
-          return;
-        }
-      });
-    });
-  }
+      listCart.appendChild(cartItem);
+    }
+  });
+  cartSpan.textContent = totalQuantity;
 };
+
 addCartToHTML();
+
+//Sign up form
+
+const signup = document.querySelector(".btn--show-modal");
