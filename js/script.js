ef537730f87a462478c63a6f4c86e5241d8eaeef
SweekarG <np05cp4s240071@iic.edu.np>
2024-05-14 11:25:27 +0545

    updated version

--- js/script.js
+++ js/script.js
@@ -450,7 +450,7 @@ const products6 = [
   },
   {
     id: 43,
-    name: "Apple Cider Vinegar",
+    name: "Apple Vinegar",
     beforePrice: 500,
     price: 300,
     image: "images/applevinegar.jpg",
@@ -628,6 +628,7 @@ const addCartToHTML = () => {
            <span>${cart.quantity}</span>
            <span class="add">></span>
          </div>
+         <svg class="trash" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" /></svg>
        </div>`;
       listCart.appendChild(cartItem);
     }
@@ -640,3 +641,26 @@ addCartToHTML();
 //Sign up form
 
 const signup = document.querySelector(".btn--show-modal");
+
+//slide on click
+
+const slideBtn = document.querySelectorAll(".slider-component .slide--img");
+const contentList = document.querySelector(
+  ".slider-component .section--featured"
+);
+const maxScrollLeft = contentList.scrollWidth - contentList.clientWidth;
+
+slideBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const direction = btn.classList.contains("back--img") ? -1 : 1;
+    const slide = contentList.clientWidth * direction;
+    contentList.scrollBy({ left: slide, behavior: "smooth" });
+    console.log("clicked");
+  });
+});
+
+const handleBtns = () => {
+  slideBtn[0].style.display = contentList.scrollLeft <= 0 ? "none" : "flex";
+  slideBtn[1].style.display =
+    contentList.scrollLeft >= maxScrollLeft ? "none" : "flex";
+};
